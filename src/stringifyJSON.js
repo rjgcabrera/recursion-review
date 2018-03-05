b37b9354d69c8ef31c3507dcb57b2a16f905ea57
rjgcabrera <rjgcengr@gmail.com>
2018-03-05 11:46:42 -0800

    Implement stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,46 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // vars
+  var objType = typeof obj;
+
+  // collections (arrays & objects)
+  if (objType === 'object') {
+    if (obj === null) {
+      return 'null';
+    }
+    else if (Array.isArray(obj)) {
+      var arrayStr = '[';
+      for (var i = 0; i < obj.length; i++) {
+        arrayStr += stringifyJSON(obj[i]) + 
+                     (i === obj.length - 1 ? '' : ',');
+      }
+      return arrayStr + ']';
+    }
+    else {
+      var objStr = '{';
+      var keyArr = Object.keys(obj);
+      var propStr = '';
+      for (var i = 0; i < keyArr.length; i++) {
+        propStr = stringifyJSON(obj[keyArr[i]]);
+        if (propStr !== '') {
+          objStr += '\"' + keyArr[i] + '\":' + propStr +
+                     (i === keyArr.length - 1 ? '' : ',');
+        }
+      }
+      return objStr + '}';
+    }
+  }
+  
+  // non-collection objs / base case
+  if (objType === 'number' ||
+      objType === 'boolean') {
+    return obj.toString();
+  }
+  else if (objType === 'string') {
+    return '\"' + obj + '\"';
+  }
+
+  return '';
+
 };
